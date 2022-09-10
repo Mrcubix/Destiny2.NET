94e35ef3c5a14e637214e89e35374f5d3a849894
Mrcubix <tintamare91@gmail.com>
2022-09-10 21:47:01 +0200

    Adding forgotten project file and Settings

---
+++ src/Settings.cs
@@ -0,0 +1,10 @@
+namespace API
+{
+    public class Settings
+    {
+        public string Key { get; set; }
+        public string Username { get; set; }
+        public short Tag { get; set; }
+        public short MaxRetries { get; set; }
+    }
+}
