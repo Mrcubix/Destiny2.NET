77bc8dfe33d321e259113cfbaafdf44ad3eec5e9
Mrcubix <tintamare91@gmail.com>
2022-09-10 21:49:19 +0200

    Add forgotten ExactSearchRequest

---
+++ src/Entities/Request/User/ExactSearchRequest.cs
@@ -0,0 +1,8 @@
+namespace API.Entities.Request.User
+{
+    public class ExactSearchRequest
+    {
+        public string displayName { get; set; }
+        public short displayNameCode { get; set; } 
+    }
+}
