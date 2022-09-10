38a0e56706dfc8116c705d3dcd24062cdd523d50
Mrcubix <tintamare91@gmail.com>
2022-09-10 21:50:56 +0200

    Implement basic retry mechanism

--- src/Endpoints/Destiny2.cs
+++ src/Endpoints/Destiny2.cs
@@ -38,7 +38,7 @@ namespace API.Endpoints
         {
             if (string.IsNullOrEmpty(settings.Key))
                 throw InvalidAPIKeyException;
-                
+
             ExactSearchRequest body = new();
             body.displayName = name;
             body.displayNameCode = tag;
@@ -56,71 +56,82 @@ namespace API.Endpoints
             HttpResponseMessage response = null;
             HttpRequestMessage request = null;
 
-            try
+            short retries = 0;
+
+            while(retries++ < settings.MaxRetries - 1)
             {
-                if (method.ToLower() == "post")
+                try
                 {
-                    if (body != null)
+                    if (method.ToLower() == "post")
                     {
-                        request = new();
-                        request.Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
-                        request.Method = new("POST");
-                        request.RequestUri = url;
+                        if (body != null)
+                        {
+                            request = new();
+                            request.Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+                            request.Method = new("POST");
+                            request.RequestUri = url;
+                        }
+                        
+                        response = await client.SendAsync(request);
                     }
-                    
-                    response = await client.SendAsync(request);
-                }
-                else if (method.ToLower() == "get")
-                {
-                    response = await client.GetAsync(url);
-                }
-            }
-            catch(Exception E)
-            {
-                Console.Clear();
-                Console.WriteLine("An Exception Occured: ");
-                Console.WriteLine(E.ToString());
-                Console.WriteLine("Press Enter to continue...");
-                Console.ReadKey();
-                return null;
-            }
+                    else if (method.ToLower() == "get")
+                    {
+                        response = await client.GetAsync(url);
+                    }
 
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadAsStringAsync();
-            }
-            else
-            {
-                Console.Clear();
-                Console.Write("An Error Occured: ");
-
-                int statusCode = (int)response.StatusCode;
-
-                if (statusCode == 520) // Bungie broke everything, again
-                {
-                    Console.WriteLine("The API returned an unexpected result");
                 }
-                else if(statusCode == 522) // Let's be real, it's more likely that Bungie's API is down
+                catch(Exception E)
                 {
-                    Console.WriteLine("The request timed out");
-                    Console.WriteLine("This may be be either due to a bad connection or the API is down");
+                    Console.Clear();
+                    Console.WriteLine("An Exception Occured: ");
+                    Console.WriteLine(E.ToString());
+                    Console.WriteLine("Press Enter to continue...");
+                    Console.ReadKey();
+                    return null;
+                }
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
                 }
                 else
                 {
-                    if (response.Content.Headers.ContentType != null) // yes, this can happen
-                    {
-                        string mediaType = response.Content.Headers.ContentType.MediaType;
+                    Console.Clear();
+                    Console.Write("An Error Occured: ");
 
-                        if (mediaType == "text/html") // Method not allowed
+                    int statusCode = (int)response.StatusCode;
+
+                    if (statusCode == 520)
+                    {
+                        Console.WriteLine("The API returned an unexpected result");
+                    }
+                    else if(statusCode == 522)
+                    {
+                        Console.WriteLine("The request timed out");
+                        Console.WriteLine("This may be be either due to a bad connection or the API is down");
+                    }
+                    else
+                    {
+                        if (response.Content.Headers.ContentType != null)
                         {
-                            Console.WriteLine("Method doesn't exist or isn't allowed");
-                            return null;
-                        }
-                        else if (mediaType == "application/json") // usually return a DestinyErrorResponse
-                        {
-                            var serializedResponse = await response.Content.ReadAsStringAsync();
-                            var apiResponse = JsonSerializer.Deserialize<DestinyErrorResponse>(serializedResponse);
-                            Console.WriteLine(apiResponse.Message);
+                            string mediaType = response.Content.Headers.ContentType.MediaType;
+
+                            if (mediaType == "text/html")
+                            {
+                                Console.WriteLine("Method doesn't exist or isn't allowed");
+                                return null;
+                            }
+                            else if (mediaType == "application/json")
+                            {
+                                var serializedResponse = await response.Content.ReadAsStringAsync();
+                                var apiResponse = JsonSerializer.Deserialize<DestinyErrorResponse>(serializedResponse);
+                                Console.WriteLine(apiResponse.Message);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Failed due to an unknown error (HTTP error code {statusCode})");
+                                return null;
+                            }
                         }
                         else
                         {
@@ -128,13 +139,12 @@ namespace API.Endpoints
                             return null;
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine($"Failed due to an unknown error (HTTP error code {statusCode})");
-                        return null;
-                    }
+
+                    Console.WriteLine($"Now retrying... (Retry: {retries})");
                 }
             }
+
+            Console.WriteLine($"Failed after {retries} retries");
             return null;
         }
     }
