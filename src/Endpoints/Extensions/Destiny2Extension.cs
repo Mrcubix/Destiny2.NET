5c3a09648831a1cbe62483a79a519d9e754f72c3
Mrcubix <tintamare91@gmail.com>
2022-09-11 02:53:19 +0200

    Implement Destiny2Extension, a class meant for extension methods, Implemented GetCurrentActivity()

---
+++ src/Endpoints/Extensions/Destiny2Extension.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+using API.Entities.Characters;
+using API.Enums;
+
+namespace API.Endpoints
+{
+    public static class Destiny2Extension
+    {
+        public static async Task<DestinyCharacterActivitiesComponent> GetCurrentActivity(this Destiny2 api, int type, long id, long characterId)
+        {
+            if (string.IsNullOrEmpty(api.settings.Key))
+                throw Destiny2.InvalidAPIKeyException;
+
+            api.cachedComponentResponse = new();
+
+            string serializedResponse = await api.SendRequest("GET", new Uri($"{Destiny2.BaseUrl}/Destiny2/{type}/Profile/{id}/Character/{characterId}/?components={DestinyComponentType.CharacterActivities}"));
+
+            if (serializedResponse == null)
+                throw Destiny2.requestProcessingErrorResponse;
+
+            return JsonSerializer.Deserialize<DestinyCharacterActivitiesComponent>(serializedResponse, api.SerializerOptions);
+        }
+    }
+}
