9c085690110c64a4b149c5a0a76f5ecab98fd430
Mrcubix <tintamare91@gmail.com>
2022-10-01 17:05:35 +0200

    Implement DestinyDefinition to DestinyClassDefinition & DestinyRaceDefinition

--- src/Entities/Definitions/DestinyClassDefinition.cs
+++ src/Entities/Definitions/DestinyClassDefinition.cs
@@ -2,15 +2,12 @@ using API.Entities.Definitions.Common;
 
 namespace API.Entities.Definitions
 {
-    public class DestinyClassDefinition
+    public class DestinyClassDefinition : DestinyDefinition
     {
         public int ClassType { get; set; }
         public DestinyDisplayPropertiesDefinition DisplayProperties { get; set; }
         public Dictionary<int, string> GenderedClassNames { get; set; }
         public Dictionary<uint, string> GenderedClassNamesByGenderHash { get; set; }
         public uint? MentorVendorHash { get; set; }
-        public uint Hash { get; set; }
-        public int Index { get; set; }
-        public bool Redacted { get; set; }
     }
 }
--- src/Entities/Definitions/DestinyRaceDefinition.cs
+++ src/Entities/Definitions/DestinyRaceDefinition.cs
@@ -2,14 +2,11 @@ using API.Entities.Definitions.Common;
 
 namespace API.Entities.Definitions
 {
-    public class DestinyRaceDefinition
+    public class DestinyRaceDefinition : DestinyDefinition
     {
         public int RaceType { get; set; }
         public DestinyDisplayPropertiesDefinition DisplayProperties { get; set; }
         public Dictionary<int, string> GenderedRaceNames { get; set; }
         public Dictionary<uint, string> GenderedRaceNamesByGenderHash { get; set; }
-        public uint Hash { get; set; }
-        public int Index { get; set; }
-        public bool Redacted { get; set; }
     }
 }
