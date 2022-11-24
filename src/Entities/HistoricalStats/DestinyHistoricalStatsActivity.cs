5c88cf630556be9ff37e9d4b350a0f1ed31ccf86
Mrcubix <tintamare91@gmail.com>
2022-11-24 15:21:14 +0100

    Forgot more occurences

--- src/Entities/HistoricalStats/DestinyHistoricalStatsActivity.cs
+++ src/Entities/HistoricalStats/DestinyHistoricalStatsActivity.cs
@@ -6,28 +6,10 @@ namespace API.Entities.HistoricalStats
         public uint ReferenceId {get; set; }
         // TODO: map Hash to Destiny.Definitions.DestinyActivityDefinition
         public uint DirectorActivityHash { get; set; }
-        private long _instanceId;
-        public string InstanceId
-        {
-            get => _instanceId.ToString();
-            set
-            {
-                long.TryParse(value, out _instanceId);
-            }
-        }
+        public long _instanceId { get; set; }
         public int Mode { get; set; }
         public int[] Modes { get; set; }
         public bool IsPrivate { get; set; }
         public int MembershipType { get; set; }
-
-        public long GetInstanceId()
-        {
-            return _instanceId;
-        }
-
-        public void SetInstanceId(long id)
-        {
-            this._instanceId = id;
-        }
     }
 }
--- src/Entities/Profiles/DestinyProfileComponent.cs
+++ src/Entities/Profiles/DestinyProfileComponent.cs
@@ -7,14 +7,7 @@ namespace API.Entities.Profiles
         public UserInfoCard UserInfo { get; set; }
         public DateTime DateLastPlayed { get; set; }
         public int VersionsOwned { get; set; }
-        private long[] _CharacterIds;
-        public string[] CharacterIds { 
-            get => _CharacterIds.Select(x => x.ToString()).ToArray();
-            set
-            {
-                _CharacterIds = value.Select(long.Parse).ToArray();
-            }
-        }
+        public long[] _CharacterIds { get; set; }
         public uint[] seasonHashes { get; set; }
         public uint[] EventCardHashesOwned { get; set; }
         public uint CurrentSeasonHash { get; set; }
